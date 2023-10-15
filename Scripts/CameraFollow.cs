4ac5981921c107f8907de8a6e1509a84dbadd5fb
kobyarcher <kobya941@gmail.com>
2023-10-15 17:36:31 +1100

    added a basic movement system with some dodgy sprites.. I also added a smoot camera and a door that loads a scene.

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Vector3 offset = new Vector3(0f, 0f, -10f);
+    float smoothTime = 0.3f;
+    Vector3 velocity = Vector3.zero;
+
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 targetPos = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
---
+++ Scripts/FadeInOut.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInOut : MonoBehaviour
+{
+    public Transform panel;
+    public float fadeTime = 0.7f;
+    [HideInInspector] public bool fadeIn = false;
+    [HideInInspector] public float timer = 0f;
+    [HideInInspector] public bool finished = false;
+
+    Image panelImg;
+    private void Start()
+    {
+        panelImg = panel.transform.GetComponent<Image>();
+    }
+    public void Fade()
+    {
+        panelImg.color = new Color(0f, 0f, 0f, Mathf.Lerp(fadeIn ? 0 : 1, fadeIn ? 1 : 0, timer / fadeTime));
+        if (timer >= fadeTime)
+        {
+            finished = true;
+
+        }
+        else
+        {
+            finished = false;
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        Fade();
+    }
+}
