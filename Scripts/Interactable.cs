bf5c7cad66b61828d24a00edbc64d89c7d93f6dc
kobyarcher <kobya941@gmail.com>
2023-11-23 09:16:56 +1100

    added a pixel art font

--- Scripts/Interactable.cs
+++ Scripts/Interactable.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.GetComponent<Player>() != null)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Player>() != null)
         {
             other.GetComponent<Player>().interactableItem = transform;
         }
     }
-    private void OnTriggerExit2D(Collider2D other) {
-        if(other.GetComponent<Player>() != null)
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<Player>() != null)
         {
             other.GetComponent<Player>().interactableItem = null;
         }
